7506af42686e943e77691d2fd2e7bcb37f3da0da
ApiratK <>
2025-12-19 09:10:14 +0700

    :package: add auth&middleware

--- modules/auth/authRepository/authRepository.go
+++ modules/auth/authRepository/authRepository.go
@@ -1,1 +1,25 @@
 package authrepository
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type (
+
+	AuthRepositoryService interface{}
+
+	authrepository struct {
+		db *mongo.Client
+	}
+)
+
+func NewAuthRepositoryService(db *mongo.Client)AuthRepositoryService {
+	return &authrepository{db}
+}
+
+
+func (r * authrepository)authDbConn(pctx context.Context) *mongo.Database{
+	return r.db.Database("auth_db")
+}
--- modules/middleware/middlewareHandler/middlewareHandler.go
+++ modules/middleware/middlewareHandler/middlewareHandler.go
@@ -1,1 +1,20 @@
 package middlewarehandler
+
+import (
+	"github.com/TewApirat/items-shop-ms/config"
+	"github.com/TewApirat/items-shop-ms/modules/middleware/middlewareUsecase"
+)
+
+type (
+	MiddlewareHandlerService interface{}
+
+	middlewareHandler struct{
+		cfg 				*config.Config
+		middleWareUsecase 	middlewareusecase.MiddlewareUsecaseService
+		
+	}
+)
+
+func NewMiddlewareHandler(cfg *config.Config, middleWareUsecase middlewareusecase.MiddlewareUsecaseService)MiddlewareHandlerService{
+	return &middlewareHandler{cfg, middleWareUsecase}
+}
