16a6c02080d06ccda3705b0b62b381f74111c3ed
ApiratK <>
2025-12-22 10:06:30 +0700

    :package: add utils

--- pkg/utils/helper.go
+++ pkg/utils/helper.go
@@ -1,1 +1,31 @@
 package utils
+
+import (
+	"log"
+	"time"
+	"encoding/json"
+	"fmt"
+)
+
+func Debug(obj any){
+	raw, _ := json.MarshalIndent(obj, "", "\t")
+	fmt.Println(string(raw))
+}
+
+func LocalTime() time.Time {
+	loc, _ := time.LoadLocation("Asia/Bangkok")
+	return time.Now().In(loc)
+}
+
+
+func ConvertStringTimetoTime(t string) time.Time {
+	layout := "2026-01-02T15:04:05.999 -0700 MST"
+	result, err := time.Parse(layout, t)
+	if err != nil {
+		log.Printf("Error: Parse time failed: %s",err.Error())
+	}
+
+
+	loc, _ := time.LoadLocation("Asia/Bangkok")
+	return result.In(loc)
+}
